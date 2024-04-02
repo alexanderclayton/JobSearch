614608606e9dfc5677e1af12250f188b05be881b
Alex <alexander.clayton.email@gmail.com>
2024-04-02 11:36:30 -0500

    addUser form, fetch request

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
+import { AddUser } from "./components";
+
 function App() {
   return (
     <>
-      <h2 className="m-2 text-center font-bold">Hello World</h2>
+      <AddUser />
     </>
   );
 }
